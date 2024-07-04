f0ffbaf1efaf4a21060095c909a1353c7130e0b7
Yangyang <1635656638@qq.com>
2024-07-04 10:11:38 +0800

    feat: 🚀 playground

---
+++ playground/vite/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Form } from '@wuhan/solid-common/Form'
+import { Input } from '@wuhan/solid-common/Input'
+import '@wuhan/solid-common/style.css'
+
+function App() {
+  return (
+    <Form>
+      <Input />
+    </Form>
+  )
+}
+
+export default App
