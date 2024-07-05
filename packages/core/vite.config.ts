934f57ab359e1988d1a68fd07763424d54b039cb
Yangyang <1635656638@qq.com>
2024-07-06 01:55:16 +0800

    feat: 🚀 rollupOptions

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -30,6 +30,9 @@ export default defineConfig(() => ({
       output: {
         preserveModules: true,
         exports: 'named',
+        format: 'es',
+        dir: 'dist',
+        preserveModulesRoot: 'src',
       },
       plugins: [terser()],
     },
