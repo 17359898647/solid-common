3539a85095b8dcd18ff17176c73debbc21b2be7a
Yangyang <1635656638@qq.com>
2024-07-08 14:12:00 +0800

    fix: 🧩 preserveModulesRoot

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig(async () => {
           format: 'es',
           dir: 'dist',
           preserveModules: true,
-          preserveModulesRoot: resolve(__dirname),
+          preserveModulesRoot: resolve(__dirname, '../..'),
         },
         plugins: [
           terser(),
