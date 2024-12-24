022e94d1db8c1962d4184f6d03f91be3a370ee48
Yangyang <yangyang@weimill.com>
2024-12-25 00:04:42 +0800

    temp

--- playground/vite/src/App.tsx
+++ playground/vite/src/App.tsx
@@ -2,58 +2,73 @@ import '@gedatou/solid-common/style.css'
 import { _get } from '@felte/common'
 import { createForm } from '@felte/solid'
 import type { ICreateStoreHelperReturn } from '@gedatou/solid-common'
-import { createStoreHelper, FelteHelpKey, useFelteHelp } from '@gedatou/solid-common'
+import { createStoreHelper, FelteHelpKey, noop, useFelteHelp } from '@gedatou/solid-common'
+import { Button } from '@kobalte/core/button'
+import type { FactoryArg, InputMask } from 'imask'
 import imask from 'imask'
-import { createEffect, onCleanup } from 'solid-js'
+import type { UpdateOpts } from 'imask/masked/factory'
+import { createEffect, createSignal, onCleanup } from 'solid-js'
 
 export enum PhoneMaskStr {
   allHidden = '(HHH)` HHH`-HHHH',
   allShow = '(SSS)` SSS`-SSSS',
 }
-function maskHelp() {
+
+export enum maskStr {
+  allHidden = '`H`H`H-`H`H-`H`H`H`H',
+  allShow = '`S`S`S`-`S`S`-`S`S`S`S`',
+  lastShow = '`H`H`H`-`H`H`-`S`S`S`S`',
+}
+
+function maskFn(val: string) {
+  if (val === ' ')
+    return false
+  if (val === '*')
+    return true
+  return !Number.isNaN(Number(val))
+}
+function maskHelp(options: FactoryArg & { name?: string }) {
   let el: HTMLInputElement
-  const formatValue = imask.createPipe({
-    mask: PhoneMaskStr.allHidden,
-    lazy: true,
-    definitions: {
-      H: {
-        mask: Number,
-        placeholderChar: '#',
-        displayChar: '*',
-      },
-      S: {
-        placeholderChar: '#',
-        mask: Number,
-      },
-    },
-  })
   let felteHelp: ICreateStoreHelperReturn
+  const [mask, setMask] = createSignal<InputMask<FactoryArg>>()
   const abort = new AbortController()
   const ref = (e: HTMLInputElement) => {
     el = e
-    const name = e.name
+    if (!el)
+      return
+    setMask(imask(el, options))
+    if (!mask())
+      return
     const parent = el.closest('form')
     const key = parent?.dataset[FelteHelpKey]
+    const { name } = options
     felteHelp = createStoreHelper(key)
     const isFelte = !!name && !!parent
     queueMicrotask(() => {
-      el.value = formatValue(el.value)
+      mask()!.value = el.value || ''
     })
-
+    el.addEventListener('focusout', () => (isFelte && felteHelp.setTouched(name, true)), {
+      signal: abort.signal,
+    })
     el.addEventListener('input', () => {
-      el.value = formatValue(el.value || '')
-      isFelte && felteHelp.setData(name, el.value)
+      isFelte && felteHelp.setData(name, mask()!.rawInputValue)
     }, {
       signal: abort.signal,
     })
-    const unsubscribe = name
+    const unsubscribe = isFelte
       ? felteHelp.subscribe(
           (data) => {
             const value = _get(data, name)
-            el.value = value ? formatValue(String(value)) : ''
+            queueMicrotask(() => {
+              mask()!.value = String(value) ?? ''
+            })
           },
         )
-      : () => {}
+      : noop
+    mask()!.on('accept', () => {
+      console.log(mask()!.rawInputValue)
+    })
+
     onCleanup(() => {
       unsubscribe?.()
       abort.abort()
@@ -61,42 +76,78 @@ function maskHelp() {
   }
   return {
     ref,
-    formatValue,
+    mask,
+    update: (options: UpdateOpts<FactoryArg>) => mask()?.updateOptions?.(options),
   }
 }
 
 function App() {
-  const { form, data, setData, touched } = createForm({
+  const { form, data } = createForm({
     initialValues: {
       phone: '123123123',
     },
     extend: [useFelteHelp],
   })
+
   createEffect(() => {
-    console.log(data(), touched())
+    console.log(data())
   })
-  const { ref, formatValue } = maskHelp()
+  let input: HTMLInputElement
+  const { ref, update, mask } = maskHelp({
+    mask: maskStr.allShow,
+    lazy: true,
+    definitions: {
+      H: {
+        mask: maskFn,
+        placeholderChar: '#',
+        displayChar: '*',
+      },
+      S: {
+        mask: maskFn,
+        placeholderChar: '#',
+      },
+    },
+    name: 'phone',
+  })
   return (
     <form
       ref={form}
       class="h--h-screen h--flex h--items-center h--justify-center"
     >
       <input
-        // ref={ref}
+        ref={ref}
         class="h--border-border h--border"
-        name="phone"
-        // value={formatValue(data('phone'))}
+        value={data('phone')}
         // onInput={(e) => {
         //   console.log(e.target.value)
         // }}
       />
       <input
         class="h--border-border h--border"
+        name="phone"
         value={data('phone')}
-        onInput={(e) => {
-          setData('phone', formatValue(e.currentTarget.value))
-        }}
       />
+      <Button
+        onClick={() => {
+          mask()?.updateOptions({
+            mask: maskStr.allHidden,
+            lazy: true,
+            definitions: {
+              H: {
+                mask: maskFn,
+                placeholderChar: '#',
+                displayChar: '*',
+              },
+              S: {
+                mask: maskFn,
+                placeholderChar: '#',
+              },
+            },
+          })
+        }}
+      >
+        change
+      </Button>
     </form>
   )
 }
